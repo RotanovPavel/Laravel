023f0f21a376c38078509860f8977803dd0746f9
ODDpavel <rotanov.pavel@gmail.com>
2019-05-23 10:09:27 +0300

    first

---
+++ resources/js/app.js
@@ -0,0 +1,80 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+//window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// const app = new Vue({
+//     el: '#app',
+// });
+
+$('#btn-top').click(function () {
+    if ($(".navbar-ex2-collapse").hasClass("in")) {
+        $('#ex-2').removeClass('in');
+    }
+});
+
+$('#btn-middle').click(function () {
+    if ($(".navbar-ex1-collapse").hasClass("in")) {
+        $('#ex-1').removeClass('in');
+    }
+});
+
+
+
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+    console.log('slider');
+    var act = $(".active p").text();
+    $('.carousel-bottom-center p').text(act);
+    $('.carousel-bottom-center p').hide();
+    $('.carousel-bottom-center p').fadeToggle(1500);
+});
+
+
+$(document).ready(function () {
+    console.log('swiper');
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 0,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        autoplay: {
+            delay: 5000
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+
+});
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="top-block">
+
+    <nav class="navbar navbar-default " role="navigation">
+
+                <div class="navbar-header v-align-m">
+
+                    <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+
+                    <a class="navbar-brand logo" href="{{ url('/') }}"><img src="{{ asset('img/Flame_31x41.png') }}">
+                        <p class="v-align-m">{{ config('app.name', 'Dynevo') }}</p><h5> so turn the light out</h5>
+                    </a>
+
+                </div>
+
+                <div class="collapse navbar-collapse navbar-ex1-collapse v-align-m " id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li >
+                                <a  href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li >
+                                    <a  href="{{ route('register') }}">{{ __('Create an account') }}</a>
+                                </li>
+                            @endif
+                        @else
+
+
+                                    <li><a href="#">wish list (0)</a></li>
+                                    <li><a href="#">my account</a></li>
+                                    <li><a href="#">shopping cart</a></li>
+                                    <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form></li>
+
+                                    <li>
+                                        <button class="button "><i class="fa fa-shopping-basket"></i></button>
+                                        <div class="circle-button">0</div>
+                                    </li>
+
+                        @endguest
+                    </ul>
+                </div>
+
+    </nav>
+</div>
+        <main class="py-4">
+            <div class="container">
+                @yield('content')
+            </div>
+
+        </main>
+
+</body>
+</html>
